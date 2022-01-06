42e434cb60400e93391d2828eb08b4f328596b0e
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-01-06 17:30:40 +0330

    Add open/close functionalities for navigation

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import Navigation from "./Navigation";
 
 const Layout = (props)=>{
   const {children} = props;
+  const [open,setOpen]=useState(false);
   return(
     <div className={`d-flex `}>
-      <Navigation/>    
+      <div onClick={()=>{setOpen(!open)}}>hello</div>
+      {open&&<Navigation/>  }  
       <div className={`flex-grow-1`}>
       {children}
       </div> 
