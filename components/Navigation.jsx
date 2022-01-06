73ce929d1eae629121e76d84f921d490192ef3b1
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-01-06 18:01:57 +0330

    Add navigation open/close

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from '../styles/Navigation.module.scss'
 
 const Navigation =(props)=>{
+   const {isOpen} = props;
   return(
 
-    <div className={`d-flex flex-column  ${styles.br}`} >
+    <div className={`d-flex flex-column  ${styles.br} ${isOpen?styles.bro:styles.brc}`} >
+      
        <div className={`d-flex ${styles.icon}`}>
-          doctors
+          profile
        </div>
        <div className={`d-flex ${styles.icon}`}>
           doctors
--- components/layout.jsx
+++ components/layout.jsx
@@ -1,13 +1,17 @@
 import React,{useState} from "react";
 import Navigation from "./Navigation";
-
+import shape from '../styles/Layout.module.scss'
 const Layout = (props)=>{
   const {children} = props;
   const [open,setOpen]=useState(false);
   return(
     <div className={`d-flex `}>
-      <div onClick={()=>{setOpen(!open)}}>hello</div>
-      {open&&<Navigation/>  }  
+      <div onClick={()=>{setOpen(!open)}} className={`d-flex align-items-center justify-content-center  ${shape.navbut} ${open?shape.opnav:shape.clnav}`}>
+        {open ?'x':'+'}
+     
+        </div>
+      {/* {open&&<Navigation/>  }   */}
+      <Navigation isOpen={open}/>
       <div className={`flex-grow-1`}>
       {children}
       </div> 
