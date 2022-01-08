9540d621b9a0b329266be1ebc1c59687fa8c67ba
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-01-08 17:57:07 +0330

    Add navigation links

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '../styles/Navigation.module.scss'
+import Link from 'next/link'
 
 const Navigation =(props)=>{
    const {isOpen} = props;
@@ -8,19 +9,37 @@ const Navigation =(props)=>{
     <div className={`d-flex flex-column  ${styles.br} ${isOpen?styles.bro:styles.brc}`} >
       
        <div className={`d-flex ${styles.icon}`}>
-          profile
+        <Link href="/profile">
+          <a> profile</a>
+        </Link>
+         
        </div>
        <div className={`d-flex ${styles.icon}`}>
-          doctors
+       <Link href="/">
+          <a> home</a>
+        </Link>
+         
        </div>
        <div className={`d-flex ${styles.icon}`}>
-          doctors
+       <Link href="/doctors">
+          <a> doctors</a>
+        </Link>
+        
        </div>
        <div className={`d-flex ${styles.icon}`}>
-          doctors
+       <Link href="/visit">
+          <a> visit</a>
+        </Link>
        </div>
        <div className={`d-flex ${styles.icon}`}>
-          doctors
+       <Link href="/transaction">
+          <a> transaction</a>
+        </Link>
+       </div>
+       <div className={`d-flex ${styles.icon}`}>
+       <Link href="/exit">
+          <a> exit</a>
+        </Link>
        </div>
     </div>
   )
