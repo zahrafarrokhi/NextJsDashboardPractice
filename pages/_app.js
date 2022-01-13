e59dbea956844c47372d45f3541bc3aa6e8df33d
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-01-13 20:03:44 +0330

    Add default _app format

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return getLayout(<Component {...pageProps} />)
+  // return <Component {...pageProps}/>
 }
 
 export default MyApp
