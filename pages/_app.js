3974e37177877b5e4458c9e9aa4d676853dd7af8
Zahra Farrokhi <zahrafarrokhi2017@gmail.com>
2022-01-20 17:01:51 +0330

    Add datepicker and react icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import "react-datepicker/dist/react-datepicker.css";
 import '../styles/globals.scss'
+
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
